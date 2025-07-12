e0788213ea2a45b7eef2004afc4694b400caebb6
radhamadhab23 <radhamadhabpattnaik23@gmail.com>
2025-07-12 14:39:29 +0530

    FEAT:Added Basic User MODEL

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const {ServerConfig,logger}=require('./config');
 const {AboutController, HomeController} = require('./controllers');
 const apiRoutes = require('./routes');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use('/api',apiRoutes);
 app.listen(ServerConfig.PORT,() =>{
     console.log(`Server is running on port ${ServerConfig.PORT}`);
